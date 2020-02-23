ce7837812960d0866625791b1084d9250248e15c
murdens <sonya.murden@gmail.com>
2020-02-23 23:42:18 +0000

    update book exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
-class Book
-# write your code here
+class Book 
+    attr_reader :title
+      
+    def title=(new_title)
+        words = new_title.split(" ").map do |word|
+            if %w(an a in the of and).include?(word)
+                word
+            else
+            word.capitalize
+            end
+        end
+        words.first.capitalize!
+          
+          @title = words.join(" ")
+    end
+      
 end
