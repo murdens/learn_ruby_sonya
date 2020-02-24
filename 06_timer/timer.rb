a5c07f366258c9da3c1cbfbed3f8a9f68081c543
murdens <sonya.murden@gmail.com>
2020-02-24 13:54:58 +0000

    final 2 exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -7,9 +7,7 @@ class Timer
   end
 
   def time_string
-    hours = @seconds/3600
-    remainder = @seconds%3600
-    sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+    Time.at(seconds).utc.strftime("%H:%M:%S")
   end
   
 end
